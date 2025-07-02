1eb13e71b8c24481f14c26696c90dd2b36105d2c
Anna Eklund <anna.eklund@yahoo.se>
2025-07-02 11:50:01 +0200

    Add project files.

---
+++ MovieApi/Models/MovieDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieApi.Models
+{
+    public class MovieDetails
+    {
+        public int Id { get; set; }
+        public string Synopsis { get; set; } = null!;
+        public string Language { get; set; } = null!;
+        public int Budget { get; set; }
+        public int MovieId { get; set; } // Foreign key
+
+        [JsonIgnore] // Prevents circular reference recursion
+        public Movie Movie { get; set; } = null!; // Navigation property
+    }
+}
---
+++ MovieApi/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieApi.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string ReviewerName { get; set; } = null!;
+        public string Comment { get; set; } = null!;
+        [Range(1, 5)]
+        public int Rating { get; set; }
+        public int MovieId { get; set; } // Foreign key
+
+        [JsonIgnore] // Prevents circular reference recursion
+        public Movie Movie { get; set; } = null!; // Navigation property
+
+    }
+}
